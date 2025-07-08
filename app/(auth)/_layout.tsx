26249113f014293d97fbec0c7e3d74a793287582
Praful Gahlot <139745050+Praful-bit@users.noreply.github.com>
2025-07-08 21:18:02 +0530

    today i create  login input

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Slot } from "expo-router";
+import { images } from "@/constants";
+import CustomInput from "@/components/Custominput";
+import CustomButton from "@/components/CustomButton";
 
 const _Layout = () => {
   return (
-    <SafeAreaView>
-      <Text>Auth layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView behavior={Platform.OS === "android" ? "padding" : "height"} >
+      <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+        <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25 }}>
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10"/>
+        </View>
+          
+       <CustomInput 
+       placeholder="Ente Your Eamil"
+       value={""}
+       onChangeText={(text)=>{}}
+       label="Email"
+       keyboardType="email-address"
+       />
+       <CustomButton/>
+      </ScrollView>
+          <Slot />
+    </KeyboardAvoidingView>
   );
 };
 
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: "com.pg.foodordering",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "686ba6950015cd406b07",
+    userCollectionId: "686ba701003d91149fd8"
+}
